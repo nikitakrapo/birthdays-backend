5ed9797d73fe9b160b3c0293c07b846c1ccd8ace
nikitakrapo <n.krapotkin@gmail.com>
2025-06-01 14:55:16 +0200

    Change DB_USER env

--- src/main/kotlin/environment/BirthdaysEnv.kt
+++ src/main/kotlin/environment/BirthdaysEnv.kt
@@ -5,6 +5,6 @@ object BirthdaysEnv {
     val dbHost get() = System.getenv("DB_HOST")
 
     val dbName get() = System.getenv("DB_NAME")
-    val dbUsername get() = System.getenv("DB_USERNAME")
+    val dbUsername get() = System.getenv("DB_USER")
     val dbPassword get() = System.getenv("DB_PASSWORD")
 }
